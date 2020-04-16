c6e7c65dfe007b93b0e41aeb2279440267c05381
geru <59687167+dks1013@users.noreply.github.com>
2020-04-16 23:16:35 +0900

    for문

---
+++ for_in_study.py
@@ -0,0 +1,22 @@
+## 1부터 10까지 다 더한 값 출력
+##sum = 0
+##for i in range(1, 11):
+##    sum += i
+##print(sum)
+
+
+## break사용해서 1,2만 출력
+##A=[1,2,3,4,5]
+##for i in A:
+##    if i == 3:
+##        break
+##    print(i)
+
+
+## continue 사용해서 3만 제외하고 출력
+##A=[1,2,3,4,5]
+##for i in A:
+##    if i == 3:
+##        continue
+##    print(i)
+
